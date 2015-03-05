8f23626acefb4b6efa30b543b5f929fd5c5c1938
Joeri Sykora <joeri@sertik.net>
2015-03-05 15:29:12 +0100

    update copyright in license where needed

--- src/main/java/com/oracle/javafx/scenebuilder/app/about/AboutWindowController.java
+++ src/main/java/com/oracle/javafx/scenebuilder/app/about/AboutWindowController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2014, Oracle and/or its affiliates.
+ * Copyright (c) 2012, 2014, Oracle and/or its affiliates, 2015, Gluon.
  * All rights reserved. Use is subject to license terms.
  *
  * This file is available and licensed under the following license:
@@ -13,7 +13,7 @@
  *  - Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the distribution.
- *  - Neither the name of Oracle Corporation nor the names of its
+ *  - Neither the name of Oracle Corporation and Gluon nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
