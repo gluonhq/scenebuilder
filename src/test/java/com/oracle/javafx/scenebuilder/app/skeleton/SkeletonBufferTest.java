2d3392cdb5565123e409407d913961b294fcd773
Joeri Sykora <joeri@lodgon.com>
2015-12-15 13:19:04 +0100

    insert correct package name based on controller defined in fxml

---
+++ src/test/java/com/oracle/javafx/scenebuilder/app/skeleton/SkeletonBufferTest.java
@@ -0,0 +1,58 @@
+package com.oracle.javafx.scenebuilder.app.skeleton;
+
+import com.oracle.javafx.scenebuilder.kit.editor.EditorController;
+import com.oracle.javafx.scenebuilder.kit.fxom.FXOMDocument;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for {@link SkeletonBuffer#toString()}.
+ */
+public class SkeletonBufferTest {
+
+    @Test
+    public void testControllerWithoutPackageName() throws IOException {
+        EditorController editorController = new EditorController();
+        final URL fxmlURL = SkeletonBufferTest.class.getResource("ControllerWithoutPackage.fxml");
+        final String fxmlText = FXOMDocument.readContentFromURL(fxmlURL);
+        editorController.setFxmlTextAndLocation(fxmlText, fxmlURL);
+
+        SkeletonBuffer skeletonBuffer = new SkeletonBuffer(editorController.getFxomDocument());
+        String skeleton = skeletonBuffer.toString();
+
+        String firstLine = skeleton.substring(0, skeleton.indexOf("\n"));
+        assertEquals("", firstLine);
+    }
+
+    @Test
+    public void testControllerWithSimplePackageName() throws IOException {
+        EditorController editorController = new EditorController();
+        final URL fxmlURL = SkeletonBufferTest.class.getResource("ControllerWithSimplePackage.fxml");
+        final String fxmlText = FXOMDocument.readContentFromURL(fxmlURL);
+        editorController.setFxmlTextAndLocation(fxmlText, fxmlURL);
+
+        SkeletonBuffer skeletonBuffer = new SkeletonBuffer(editorController.getFxomDocument());
+        String skeleton = skeletonBuffer.toString();
+
+        String firstLine = skeleton.substring(0, skeleton.indexOf("\n"));
+        assertEquals("package com;", firstLine);
+    }
+
+    @Test
+    public void testControllerWithAdvancedPackageName() throws IOException {
+        EditorController editorController = new EditorController();
+        final URL fxmlURL = SkeletonBufferTest.class.getResource("ControllerWithAdvancedPackage.fxml");
+        final String fxmlText = FXOMDocument.readContentFromURL(fxmlURL);
+        editorController.setFxmlTextAndLocation(fxmlText, fxmlURL);
+
+        SkeletonBuffer skeletonBuffer = new SkeletonBuffer(editorController.getFxomDocument());
+        String skeleton = skeletonBuffer.toString();
+
+        String firstLine = skeleton.substring(0, skeleton.indexOf("\n"));
+        assertEquals("package com.example.app.view;", firstLine);
+    }
+}
