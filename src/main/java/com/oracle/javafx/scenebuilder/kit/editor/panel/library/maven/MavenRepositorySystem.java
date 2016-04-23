c019ccb8a4fd0271dcb9e78ebdc1483709c0d74d
jose.pereda <jose.pereda@gluonhq.com>
2016-04-23 16:12:45 +0200

    SB-32: Artifacts are resolved first to temporal folder, then installed into .m2. Remote Metadata is removed

--- src/main/java/com/oracle/javafx/scenebuilder/kit/editor/panel/library/maven/MavenRepositorySystem.java
+++ src/main/java/com/oracle/javafx/scenebuilder/kit/editor/panel/library/maven/MavenRepositorySystem.java
@@ -3,12 +3,16 @@ package com.oracle.javafx.scenebuilder.kit.editor.panel.library.maven;
 import com.oracle.javafx.scenebuilder.app.AppPlatform;
 import com.oracle.javafx.scenebuilder.app.i18n.I18N;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.codehaus.plexus.util.FileUtils;
 import org.eclipse.aether.AbstractRepositoryListener;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositoryEvent;
@@ -19,6 +23,10 @@ import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.graph.DependencyFilter;
 import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.installation.InstallRequest;
+import org.eclipse.aether.installation.InstallationException;
+import org.eclipse.aether.metadata.DefaultMetadata;
+import org.eclipse.aether.metadata.Metadata;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactRequest;
@@ -55,6 +63,8 @@ public class MavenRepositorySystem {
     
     private DefaultRepositorySystemSession session;
     
+    private LocalRepository localRepo;
+    
     private VersionRangeResult rangeResult;
     
     public MavenRepositorySystem() {
@@ -77,7 +87,7 @@ public class MavenRepositorySystem {
         
         session = MavenRepositorySystemUtils.newSession();
 
-        LocalRepository localRepo = new LocalRepository(new File(AppPlatform.getUserM2Repository()));
+        localRepo = new LocalRepository(new File(AppPlatform.getUserM2Repository()));
         session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
 
         // TODO: log file transfers
@@ -130,29 +140,95 @@ public class MavenRepositorySystem {
         rangeRequest.setRepositories(getRepositories());
         try {
             rangeResult = system.resolveVersionRange(session, rangeRequest);
+            
+            cleanMetadata(artifact);
+            
             return rangeResult.getVersions();
         } catch (VersionRangeResolutionException ex) { } 
         return new ArrayList<>();
     }
     
-    public String resolveArtifacts(RemoteRepository remoteRepository, Artifact... artifact) {
-        return Stream.of(artifact)
-            .map(a -> {
-                ArtifactRequest artifactRequest = new ArtifactRequest();
-                artifactRequest.setArtifact(a);
-                artifactRequest.setRepositories(Arrays.asList(remoteRepository));
-                return artifactRequest;
-            })
-            .map(ar -> {
-                try {
-                     ArtifactResult result = system.resolveArtifact(session, ar);
-                     return result.getArtifact().getFile().getAbsolutePath();
-                } catch (ArtifactResolutionException ex) { }
-                return "";
-            })
-            .findFirst().get();  // returns path of Jar
+    private void cleanMetadata(Artifact artifact) {
+        final String path = localRepo.getBasedir().getAbsolutePath() + File.separator
+                        + artifact.getGroupId().replaceAll("\\.", File.separator) + File.separator
+                        + artifact.getArtifactId() + File.separator;
+        final DefaultMetadata metadata = new DefaultMetadata("maven-metadata.xml", Metadata.Nature.RELEASE);
+        getRepositories()
+            .stream()
+            .map(r -> session.getLocalRepositoryManager().getPathForRemoteMetadata(metadata, r, ""))
+            .forEach(s -> {
+                File file = new File(path + s);
+                if (file.exists()) {
+                    try {
+                        Files.delete(file.toPath());
+                        Files.delete(new File(path + s + ".sha1").toPath());
+                    } catch (IOException ex) { }
+                }
+            });
     }
-    
+        
+    public String resolveArtifacts(RemoteRepository remoteRepository, Artifact... artifact) {
+        
+        LocalRepository localTmpRepo = new LocalRepository(AppPlatform.getTempM2Repository());
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localTmpRepo));
+        
+        List<Artifact> artifacts = Stream.of(artifact)
+                .map(a -> {
+                    ArtifactRequest artifactRequest = new ArtifactRequest();
+                    artifactRequest.setArtifact(a);
+                    artifactRequest.setRepositories(Arrays.asList(remoteRepository));
+                    return artifactRequest;
+                })
+                .map(ar -> {
+                    try {
+                        ArtifactResult result = system.resolveArtifact(session, ar);
+                        return result.getArtifact();
+                    } catch (ArtifactResolutionException ex) { }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()); 
+        
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
+        
+        List<File> sha1Files = null;
+        if (artifacts != null && !artifacts.isEmpty()) {
+            sha1Files = artifacts.stream()
+                .filter(a -> a.getFile() != null)
+                .map(a -> new File(a.getFile().getAbsolutePath().concat(".sha1")))
+                .collect(Collectors.toList());
+        
+            InstallRequest installRequest = new InstallRequest();
+            installRequest.setArtifacts(artifacts);
+            try {
+                system.install(session, installRequest);
+            } catch (InstallationException ex) { }
+        } 
+
+        // return path from local m2
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(artifact[0]);
+        String absolutePath = "";
+        try {
+            final File jarFile = system.resolveArtifact(session, artifactRequest).getArtifact().getFile();
+            absolutePath = jarFile.getAbsolutePath();
+            if (sha1Files != null) {
+                sha1Files.stream()
+                    .forEach(f -> {
+                        try {
+                            FileUtils.copyFile(f, new File(jarFile.getParent() + File.separator + f.getName()));
+                        } catch (IOException ex) { }
+                    });
+            }
+        } catch (ArtifactResolutionException ex) { }
+        
+        try {
+            FileUtils.deleteDirectory(AppPlatform.getTempM2Repository());
+        } catch (IOException ex) { }
+        
+        return absolutePath;
+    }
+        
     public String resolveDependencies(RemoteRepository remoteRepository, Artifact artifact) {
         DependencyFilter classpathFlter = DependencyFilterUtils.classpathFilter(JavaScopes.COMPILE);
         CollectRequest collectRequest = new CollectRequest();
@@ -161,7 +237,8 @@ public class MavenRepositorySystem {
 
         DependencyRequest dependencyRequest = new DependencyRequest(collectRequest, classpathFlter);
         try {
-            List<ArtifactResult> artifactResults = system.resolveDependencies(session, dependencyRequest).getArtifactResults();
+            List<ArtifactResult> artifactResults = system.resolveDependencies(session, dependencyRequest)
+                    .getArtifactResults();
             
             return artifactResults.stream()
                     .skip(1) // exclude jar itself
