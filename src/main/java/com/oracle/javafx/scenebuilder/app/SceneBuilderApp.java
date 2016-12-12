e608c3945d286d118f275ffe7859ac46e04ce747
erwin.morrhey <erwin@lodgon.com>
2016-12-12 11:57:04 +0100

    prepare version 8.3.0

--- src/main/java/com/oracle/javafx/scenebuilder/app/SceneBuilderApp.java
+++ src/main/java/com/oracle/javafx/scenebuilder/app/SceneBuilderApp.java
@@ -82,7 +82,7 @@ import javafx.stage.Stage;
  *
  */
 public class SceneBuilderApp extends Application implements AppPlatform.AppNotificationHandler {
-    public static final String VERSION = "8.2.0";
+    public static final String VERSION = "8.3.0";
 
     public enum ApplicationControlAction {
 
