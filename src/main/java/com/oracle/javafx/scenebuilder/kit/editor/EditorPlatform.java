3c4c2ce18efe8a32e8ca10beb71048a6a50425db
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2016-12-29 23:23:08 +0000

    fix some merge bugs

--- src/main/java/com/oracle/javafx/scenebuilder/kit/editor/EditorPlatform.java
+++ src/main/java/com/oracle/javafx/scenebuilder/kit/editor/EditorPlatform.java
@@ -77,11 +77,6 @@ public class EditorPlatform {
      * Gluon Glisten package
      */
     public static final String GLUON_PACKAGE = "com.gluonhq.charm.glisten";
-    
-    /**
-     * Gluon Glisten package
-     */
-    public static final String GLUON_PACKAGE = "com.gluonhq.charm.glisten";
 
     /**
      * This URL is where you go when the user takes Scene Builder Help action (shortcut F1)
