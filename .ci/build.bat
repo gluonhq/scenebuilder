6ebf549c7254da3f357cbb91279aa9f20ea12bb8
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-06-14 22:21:27 +0200

    only build installer on tag

--- .ci/build.bat
+++ .ci/build.bat
@@ -5,8 +5,8 @@ call gradlew check shadowJar -PVERSION=%APPVEYOR_REPO_TAG_NAME%
 echo "TAG NAME"
 echo %APPVEYOR_REPO_TAG_NAME%
 
-REM if NOT "%APPVEYOR_REPO_TAG_NAME%" == "" (
+if NOT "%APPVEYOR_REPO_TAG_NAME%" == "" (
   set VERSION=%APPVEYOR_REPO_TAG_NAME%
   echo %VERSION%
   call .ci\windows.bat
-REM )
+)
