f61c9e08939930b1d13492b509c5ab55cfe3d6d3
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-06-20 10:40:31 +0200

    test build installer script

--- .ci/build.sh
+++ .ci/build.sh
@@ -8,6 +8,7 @@ else
 fi
 
 # Check if tag is present and run bundle script
+export TRAVIS_TAG=8.5.1
 if [ -n "${TRAVIS_TAG}" ]; then
   export VERSION=${TRAVIS_TAG};
   chmod +x .ci/${TRAVIS_OS_NAME}.sh;
