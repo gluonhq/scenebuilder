fefd3d3ac7649614b767102f1ffb8d4bf8d0b0db
mimmoz81 <luca.domenic@gmail.com>
2019-04-26 09:48:21 +0200

    CHG changed message for folder hyperlink ADD tooltip for folder hyperlink

--- kit/src/main/java/com/oracle/javafx/scenebuilder/kit/editor/panel/library/manager/LibraryDialogController.java
+++ kit/src/main/java/com/oracle/javafx/scenebuilder/kit/editor/panel/library/manager/LibraryDialogController.java
@@ -67,8 +67,9 @@ import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -80,10 +81,9 @@ public class LibraryDialogController extends AbstractFxmlWindowController {
 
     @FXML
     private ListView<DialogListItem> libraryListView;
-    
     @FXML
-    private Button manageButton;
-
+    private Hyperlink classesLink;
+    
     private final EditorController editorController;
     private final UserLibrary userLibrary;
     private final Stage owner;
@@ -110,6 +110,13 @@ public class LibraryDialogController extends AbstractFxmlWindowController {
         this.preferencesControllerBase = preferencesController;
     }
 
+    @Override
+    protected void controllerDidLoadFxml() {
+    	super.controllerDidLoadFxml();
+    	
+    	this.classesLink.setTooltip(new Tooltip(I18N.getString("library.dialog.hyperlink.tooltip")));
+    }
+    
     @Override
     protected void controllerDidCreateStage() {
         if (this.owner == null) {
