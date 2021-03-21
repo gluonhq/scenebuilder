16e1569dc2152edaef9c069a863548dd04c10287
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-03-21 12:14:40 +0530

    Build Scene Builder using Maven

---
+++ .github/scripts/jpackage.sh
@@ -0,0 +1,21 @@
+jdeps_modules=$(jdeps --module-path $JAVAFX_HOME --print-module-deps --ignore-missing-deps app/target/SceneBuilder-$TAG-all.jar)
+$JAVA_HOME/bin/jlink \
+--module-path $JAVAFX_HOME \
+--add-modules $jdeps_modules \
+--output app/target/runtime \
+--strip-debug --compress 2 --no-header-files --no-man-pages
+
+$JAVA_HOME/bin/jpackage \
+--app-version $TAG \
+--input app/target \
+--install-dir /opt \
+--license-file LICENSE.txt \
+--main-jar SceneBuilder-$TAG-all.jar \
+--main-class $MAIN_CLASS \
+--name SceneBuilder \
+--description "Scene Builder" \
+--vendor Gluon \
+--verbose \
+--runtime-image app/target/runtime \
+--dest app/target \
+"$@"
