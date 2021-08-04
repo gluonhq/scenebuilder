6f4a1c4cd711f5b24667704224c1d98a88bb714d
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2021-08-04 17:19:40 +0200

    SceneBuilder lib path is now hard coded into /opt/scenebuilder.

--- .github/scripts/jpackage.sh
+++ .github/scripts/jpackage.sh
@@ -18,6 +18,6 @@ $JPACKAGE_HOME/bin/jpackage \
 --vendor Gluon \
 --verbose \
 --runtime-image app/target/runtime \
---java-options "-Djava.library.path=$APPDIR/../runtime/bin:$APPDIR/../runtime/lib" \
+--java-options "-Djava.library.path=/opt/scenebuilder/lib/runtime/bin:/opt/scenebuilder/lib/runtime/lib" \
 --dest $INSTALL_DIR \
 "$@"
