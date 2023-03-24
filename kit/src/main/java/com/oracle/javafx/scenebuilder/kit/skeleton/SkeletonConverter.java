5ce9f65b7411eaad8f45d2788c95b9c55fa1aec3
Patrick Plenefisch <simonpatp@gmail.com>
2023-03-24 16:13:16 -0400

    feat: Add JRuby controller skeleton generation (#596)

--- kit/src/main/java/com/oracle/javafx/scenebuilder/kit/skeleton/SkeletonConverter.java
+++ kit/src/main/java/com/oracle/javafx/scenebuilder/kit/skeleton/SkeletonConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2023, Gluon and/or its affiliates.
+ * Copyright (c) 2023, Gluon and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
  * This file is available and licensed under the following license:
@@ -31,72 +31,7 @@
  */
 package com.oracle.javafx.scenebuilder.kit.skeleton;
 
-class SkeletonSettings {
-
-    private LANGUAGE language = LANGUAGE.JAVA;
-    private TEXT_TYPE textType = TEXT_TYPE.WITHOUT_COMMENTS;
-    private FORMAT_TYPE textFormat = FORMAT_TYPE.COMPACT;
-
-    enum LANGUAGE {
-        JAVA("Java", ".java"), KOTLIN("Kotlin", ".kt"), JRUBY("JRuby", ".rb");
-
-        private final String name;
-        private final String ext;
-
-        LANGUAGE(String name, String fileNameExt) {
-            this.name = name;
-            this.ext = fileNameExt;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-
-        String getExtension() {
-            return ext;
-        }
-    }
-
-    enum TEXT_TYPE {
-        WITH_COMMENTS, WITHOUT_COMMENTS
-    }
-
-    enum FORMAT_TYPE {
-        COMPACT, FULL
-    }
-
-    void setLanguage(LANGUAGE language) {
-        this.language = language;
-    }
-
-    public LANGUAGE getLanguage() {
-        return language;
-    }
-
-    void setTextType(TEXT_TYPE type) {
-        this.textType = type;
-    }
-
-    SkeletonSettings withTextType(TEXT_TYPE type) {
-        this.textType = type;
-        return this;
-    }
-
-    void setFormat(FORMAT_TYPE format) {
-        this.textFormat = format;
-    }
-
-    SkeletonSettings withFormat(FORMAT_TYPE format) {
-        this.textFormat = format;
-        return this;
-    }
-
-    boolean isWithComments() {
-        return textType == TEXT_TYPE.WITH_COMMENTS;
-    }
-
-    boolean isFull() {
-        return textFormat == FORMAT_TYPE.FULL;
-    }
+@FunctionalInterface
+public interface SkeletonConverter {
+    String createFrom(SkeletonContext context);
 }
