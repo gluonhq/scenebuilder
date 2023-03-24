7ecb106a5419e8b9f750827b6bd33d2dbe5778a4
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2023-03-25 01:32:59 +0530

    Update license year

--- kit/src/test/java/com/oracle/javafx/scenebuilder/kit/skeleton/SkeletonBufferJRubyTest.java
+++ kit/src/test/java/com/oracle/javafx/scenebuilder/kit/skeleton/SkeletonBufferJRubyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022, Gluon and/or its affiliates.
+ * Copyright (c) 2023, Gluon and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
  * This file is available and licensed under the following license:
