fe33457d94769fb299f288d45d6b74e2f1663e20
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2024-04-04 21:53:46 +0200

    Editing incl. component browsing works as intended.

--- app/src/main/java/com/oracle/javafx/scenebuilder/app/DocumentWindowController.java
+++ app/src/main/java/com/oracle/javafx/scenebuilder/app/DocumentWindowController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2022, Gluon and/or its affiliates.
+ * Copyright (c) 2016, 2024, Gluon and/or its affiliates.
  * Copyright (c) 2012, 2014, Oracle and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
@@ -397,7 +397,7 @@ public class DocumentWindowController extends AbstractFxmlWindowController {
     public void loadFromFile(File fxmlFile) throws IOException {
         final URL fxmlURL = fxmlFile.toURI().toURL();
         final String fxmlText = FXOMDocument.readContentFromURL(fxmlURL);
-
+        
         FXOMDocumentSwitch[] options = FXOMDocumentSwitch.PRESERVE_UNRESOLVED_IMPORTS
                                                          .fromToggle(getPreferencesRecordGlobal().isPreserveUnresolvedImports());
 
