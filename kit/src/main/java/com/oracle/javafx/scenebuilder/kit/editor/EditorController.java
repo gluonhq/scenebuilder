66f7336acefcef0b6c5096078c746c625046bcda
Oliver Löffler <Oliver-Loeffler@users.noreply.github.com>
2025-10-03 23:25:54 +0200

    Updated copyright date.

--- kit/src/main/java/com/oracle/javafx/scenebuilder/kit/editor/EditorController.java
+++ kit/src/main/java/com/oracle/javafx/scenebuilder/kit/editor/EditorController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2024, Gluon and/or its affiliates.
+ * Copyright (c) 2017, 2025, Gluon and/or its affiliates.
  * Copyright (c) 2012, 2014, Oracle and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
